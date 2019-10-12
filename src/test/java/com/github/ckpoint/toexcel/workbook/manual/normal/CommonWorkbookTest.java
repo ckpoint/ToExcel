0e18a64d325e24e893b02eced5b4f3b3defc017c
LenKIM <joenggyu0@gmail.com>
2019-10-12 19:58:01 +0900

    fix: remove not important remark code

--- src/test/java/com/github/ckpoint/toexcel/workbook/manual/normal/CommonWorkbookTest.java
+++ src/test/java/com/github/ckpoint/toexcel/workbook/manual/normal/CommonWorkbookTest.java
@@ -18,40 +18,42 @@ public class CommonWorkbookTest {
 
     @Test
     public void create_workbook_with_type_and_fis_test() throws IOException {
+
         File file = new File("target/excel/map/read_test_1.xlsx");
 
         ToWorkBook workBook = new ToWorkBook(WorkBookType.XSSF, new FileInputStream(file));
         ToWorkSheet sheet = workBook.getSheetAt(0);
+
         Assert.assertEquals(sheet.getRowCount(), 101);
     }
 
     @Test(expected = OfficeXmlFileException.class)
     public void create_workbook_when_type_and_fis_not_equal_test() throws IOException {
+
         File file = new File("target/excel/map/read_test_1.xlsx");
 
         ToWorkBook workBook = new ToWorkBook(WorkBookType.HSSF, new FileInputStream(file));
+
         fail("No equals Type file");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void create_fail_when_no_existed_file_workbook_test() throws IOException {
-        //Given
+
         File file = new File("no/existed/file.txt");
-        //When
+
         ToWorkBook workBook = new ToWorkBook(file);
-        ToWorkSheet sheet = workBook.createSheet();
-        //Then
+
         fail("No existed file");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void create_fail_when_file_is_Directory_workbook_test() throws IOException {
-        //Given
+
         File file = new File("no/existed/");
-        //When
+
         ToWorkBook workBook = new ToWorkBook(file);
-        workBook.createSheet();
-        //Then
+
         fail("No existed file");
     }
 }
